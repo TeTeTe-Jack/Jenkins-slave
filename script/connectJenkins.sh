545dfd49276a036ba1b2b327b4a0957b4c8a99bf
administrator <administrator@localhost.localdomain>
2019-08-22 22:41:12 +0900

    add script

--- script/connectJenkins.sh
+++ script/connectJenkins.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+name=$2
+PORT=$3
+secret=$4
+workDir=$5
+jenkinsURL=$6
 
+# create a file of jenkins slave service
+echo java -jar /root/agent.jar -jnlpUrl $jenkinsUrl/computer/$name/slave-agent.jnlp -secret $secret -workDir $workDir > jenkins-slave.sh
 
-docker -H=tcp://localhost:$PORT exec -it $name java -jar /root/agent.jar -jnlpUrl $jenkinsUrl/computer/$name/slave-agent.jnlp -secret $secret -workDir $workDir
+# copy the file
+docker -H=tcp://localhost:$PORT cp jenkins-slave.sh $name:/root/
+
+# change owner and add +x
+docker -H=tcp://localhost:$PORT exec -it $name chown root:root /root/jenkins-slave.sh
+docker -H=tcp://localhost:$PORT exec -it $name chmod +x /root/jenkins-slave.sh
+
+# start service
+docker -H=tcp://localhost:$PORT exec -it $name sh /root/jenkins-slave.sh
+
+# delete created file
+rm -f jenkins-slave.sh
+
+exit 0
