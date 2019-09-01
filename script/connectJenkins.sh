e18e188644bb47a950bf2a57492e120316417ad8
administrator <administrator@localhost.localdomain>
2019-09-01 11:32:51 +0900

    modify args

--- script/connectJenkins.sh
+++ script/connectJenkins.sh
@@ -7,21 +7,21 @@ workDir=$5
 jenkinsURL=$6
 
 # create a file of jenkins slave service
-echo java -jar /root/agent.jar -jnlpUrl $jenkinsUrl/computer/$name/slave-agent.jnlp -secret $secret -workDir $workDir > jenkins-slave.sh
+echo java -jar /root/agent.jar -jnlpUrl ${jenkinsURL}computer/${name}/slave-agent.jnlp -secret ${secret} -workDir ${workDir} > jenkins-slave.sh
 
 # get agent.jar
-wget $jenkinsUrl/jnlpJars/agent.jar
+wget ${jenkinsURL}jnlpJars/agent.jar
 
 # copy the file
-docker -H=tcp://localhost:$PORT cp jenkins-slave.sh $name:/root/
-docker -H=tcp://localhost:$PORT cp agent.jar $name:/root/
+docker -H=tcp://localhost:${PORT} cp jenkins-slave.sh ${name}:/root/
+docker -H=tcp://localhost:${PORT} cp agent.jar ${name}:/root/
 
 # change owner and add +x
-docker -H=tcp://localhost:$PORT exec -it $name chown root:root /root/jenkins-slave.sh
-docker -H=tcp://localhost:$PORT exec -it $name chmod +x /root/jenkins-slave.sh
+docker -H=tcp://localhost:${PORT} exec -it ${name} chown root:root /root/jenkins-slave.sh
+docker -H=tcp://localhost:${PORT} exec -it ${name} chmod +x /root/jenkins-slave.sh
 
 # start service
-docker -H=tcp://localhost:$PORT exec -it $name sh /root/jenkins-slave.sh
+docker -H=tcp://localhost:${PORT} exec -it ${name} sh /root/jenkins-slave.sh
 
 # delete created file
 rm -f jenkins-slave.sh
