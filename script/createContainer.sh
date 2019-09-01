f790d5ddd9c5b424db62a51491c6a4f7f9bab924
administrator <administrator@localhost.localdomain>
2019-09-01 13:51:48 +0900

    remove setting IP

--- script/createContainer.sh
+++ script/createContainer.sh
@@ -2,13 +2,12 @@
 
 # set parameters
 name=$2
-IP=$3
-PORT=$4
+PORT=$3
 
 # check parameters
-portTest=`docker -H=tcp://localhost:$PORT ps 2>&1 >/dev/null`
-if test "$porTest" != '';then
-  echo error:$portTest
+portTest=`docker -H=tcp://localhost:${PORT} ps 2>&1 >/dev/null`
+if test "${porTest}" != '';then
+  echo error:${portTest}
   echo ${PORT} is not the port of docker api.
   exit 1
 fi
@@ -19,13 +18,7 @@ if test "${nameTest}" = "${name}";then
   exit 2
 fi
 
-pingTest=`ping -c 1 $IP| grep loss`
-if test "$pingTest" = '1 packets transmitted, 1 received, 0% packet loss, time 0ms';then
-  echo ${IP} is already exist.
-  exit 3
-fi
-
 # create a new container
-docker -H=tcp://localhost:$PORT run -it --restart=always --name $name --ip=$IP --privileged -d jenkins-slave-base /sbin/init
+docker -H=tcp://localhost:${PORT} run -it --restart=always --name ${name} --privileged -d jenkins-slave-base /sbin/init
 
 exit 0
