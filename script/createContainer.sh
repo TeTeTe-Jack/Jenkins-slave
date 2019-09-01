e253de77d1ecfba8b104b09adfa6b61b63800499
administrator <administrator@localhost.localdomain>
2019-09-01 11:14:59 +0900

    modify error check

--- script/createContainer.sh
+++ script/createContainer.sh
@@ -6,21 +6,21 @@ IP=$3
 PORT=$4
 
 # check parameters
-portTest=`docker -H=tcp://localhost:$PORT ps | grep "Is the docker daemon running?"`
-if [$portTest != ''];then
+portTest=`docker -H=tcp://localhost:$PORT ps 2>&1 >/dev/null`
+if test "$porTest" != '';then
   echo error:$portTest
   echo $PORT is not the port of docker api.
   exit 1
 fi
 
-nameTest=`docker -H=tcp://localhost:$PORT ps -a -f name=$name | grep -e \s*${name}$`
-if [$nameTest != ''];then
+nameTest=`docker -H=tcp://localhost:$PORT ps -a -f name=$name | grep -e \s*${name}$ 2>1& >/dev/null`
+if test "$nameTest" != '';then
   echo $name is already exist.
   exit 2
 fi
 
-pingTest=`ping -c 1 172.17.0.3| grep loss`
-if [$pingTest = '1 packets transmitted, 1 received, 0% packet loss, time 0ms'];then
+pingTest=`ping -c 1 $IP| grep loss`
+if test "$pingTest" = '1 packets transmitted, 1 received, 0% packet loss, time 0ms';then
   echo $IP is already exist.
   exit 3
 fi
